eb6482f7e06dd4139070c031937b03532b6db738
Ebere Emelike <ebere.emelike@gmail.com>
2022-08-09 15:07:29 +0100

    Released v1.0

---
+++ Chap03dBetting/Guy.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Chap03dBetting
+{
+    /// <summary>
+    /// Calculate and transfer a specified amount between to Guy objects
+    /// </summary>
+    internal class Guy
+    {
+        private string name;
+        private int cash;
+
+        public string Name { get => name; set => name = value; }
+        public int Cash { get => cash; set => cash = value; }
+
+        /// <summary>
+        /// Write the name and amount of cash of each Guy object
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            WriteLine($"{Name} has {Cash} Naira.");
+        }
+
+        /// <summary>
+        /// Give a specified amount to the other Guy object
+        /// </summary>
+        /// <param name="amount">Amount to be given</param>
+        /// <returns>Amount given</returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                WriteLine($"{Name} says: {amount} isn't a valid amount.");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                WriteLine($"{Name} says: I don't have enough cash to give you {amount} Naira.");
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+
+        /// <summary>
+        /// Receive a specified amount from the other Guy object
+        /// </summary>
+        /// <param name="amount">Amount received</param>
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                WriteLine($"{Name} says: This isn't a valid amount I'll take!)");
+            }
+            else
+            {
+                Cash += amount;
+            }
+        }
+    }
+}
--- Chap03dBetting/Program.cs
+++ Chap03dBetting/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace Chap03dBetting
+﻿using static System.Console;
+
+namespace Chap03dBetting
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new();
+            double odds = 0.75;
+            Guy player = new() { Name = "The player", Cash = 100 };
+
+            WriteLine($"Welcome esteemed client to the House. Today's odds is {odds}");
+
+            while (true)
+            {
+                WriteLine($"{player.Name} has {player.Cash} Naira");
+                placeBet:
+                WriteLine($"How much money do you want to bet? If not betting, enter a blank line");
+                string howMuch = ReadLine();
+                if (string.IsNullOrEmpty(howMuch))
+                {
+                    return;
+                }
+
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    if (amount > player.Cash)
+                    {
+                        WriteLine($"Your cash is {player.Cash}. You don't have enough cash to place this bet\n");
+                        goto placeBet;
+                    }
+                    if (amount <= 0)
+                    {
+                        WriteLine("Enter a valid amount!\n");
+                        goto placeBet;
+                    }
+                    int pot = player.GiveCash(amount) * 2;
+                    double bet = random.NextDouble();
+                    if (bet > odds)
+                    {
+                        player.ReceiveCash(pot);
+                        WriteLine($"Bet won! You have won {pot} Naira.\n");
+                    }
+                    else
+                    {
+                        WriteLine("You lost your bet!\n");
+                        if (player.Cash == 0)
+                        {
+                            WriteLine($"You don't have anymore cash to place bet: Available cash = {player.Cash}.");
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    WriteLine($"Enter a valid amount!\n");
+                    goto placeBet;
+                }
+            }
         }
     }
 }
